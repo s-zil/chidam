0009c0bd944c6cba0356a7a25e108ff9fb68d03a
s-zil <siam.zilna@gmail.com>
2026-07-14 03:12:25 +0530

    Create sam.h

---
+++ sam.h
@@ -0,0 +1,12 @@
+#ifndef HAIR_PROTOCOLS_H
+#define HAIR_PROTOCOLS_H
+
+#define PABA "A member of the B-vitamin complex that acts as a precursor for catalase production. (PABA pen B-vitamin complex sung a um ahia, catalase pianna dia sil poimo ahi)"
+#define MELANOCYTE "A specialized cell in the hair follicle that produces melanin pigment. (Mihing sam zung a cell poimo khat ahia, rong vom a piang sah hi)"
+#define HPO "Hydrogen peroxide, a molecule that accumulates in the hair follicle and bleaches pigment. (Tami hydrogen peroxide in mihing sam a kaang sah hi)"
+
+#define CHABI "1. PABA: Para-aminobenzoic acid \n" \
+              "2. MELANOCYTE: Pigment-producing cell (Rong bawldoh cell) \n" \
+              "3. HPO: Hydrogen peroxide (Sam kaang sah tu)"
+
+#endif
